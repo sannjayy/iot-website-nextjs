c5b4abe14c7ac8354e6153b3a1bd545db59bbc05
Sanjay <znasofficial@gmail.com>
2021-06-10 14:09:10 +0530

    State Added on switch

--- components/switch/SwitchCard.js
+++ components/switch/SwitchCard.js
@@ -1,17 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../../styles/Switch.module.css'
 import Link from 'next/link'
 import axios from 'axios'
 export default function SwitchCard({ data }) {
-
+    const [power, setPower] = useState(data.state)
     const clickEvent = async (id, state) => {
         let new_state 
 
         if (state == 0){
             new_state = 1
+            setPower(1)
         }
         else{
             new_state =0
+            setPower(0)
         }
         
         await axios.patch(`https://znas-iot.herokuapp.com/v1/backend/devices/${id}`, { state: new_state },)
@@ -21,7 +23,7 @@ export default function SwitchCard({ data }) {
         <Link href="/switch">
             <a className={styles.card} onClick={() => clickEvent(data.id, data.state)} >
                 <h3>{data.device_name}</h3>
-                <p>{data.state == 1 ? 'Off' : 'On'}</p>
+                <p>{power == 1 ? 'Off' : 'On'}</p>
             </a>
         </Link>
     )
